bd2e387775a0dfd10dd8f2a1d211075ba0fa0520
Trevor Whittingham <trevorw@ziprecruiter.com>
2022-09-22 12:45:41 -0600

    Initial Commit

---
+++ Async Await Demo/Async_Await_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_DemoApp.swift
+//  Async Await Demo
+//
+//  Created by Trevor Whittingham on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
